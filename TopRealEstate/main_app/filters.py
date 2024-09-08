4ec90256f146ca3c794d414191ba353672b4ef2d
VasiliyGubenkov <v.gubenkov@gmail.com>
2024-09-08 13:13:48 +0200

    development18

--- TopRealEstate/main_app/filters.py
+++ TopRealEstate/main_app/filters.py
@@ -1,13 +1,19 @@
 import django_filters
 from .models import Advert
 
+import django_filters
+from .models import Advert
+
 
 class AdvertFilter(django_filters.FilterSet):
     title = django_filters.CharFilter(field_name='title', lookup_expr='icontains')
     description = django_filters.CharFilter(field_name='description', lookup_expr='icontains')
 
-
     price = django_filters.RangeFilter(field_name='price', label="Цена (от и до)")
+
+    address_city_name = django_filters.CharFilter(field_name='address_city_name', lookup_expr='icontains')
+    address_street_name = django_filters.CharFilter(field_name='address_street_name', lookup_expr='icontains')
+    address_house_number = django_filters.CharFilter(field_name='address_house_number', lookup_expr='icontains')
 
     class Meta:
         model = Advert
