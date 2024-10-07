ca1f9c0982a7d713503760b533cfdfd64992be36
VasiliyGubenkov <v.gubenkov@gmail.com>
2024-10-07 20:06:27 +0200

    development72

--- TopRealEstate/TopRealEstate/wsgi.py
+++ TopRealEstate/TopRealEstate/wsgi.py
@@ -1,16 +1,9 @@
-"""
-WSGI config for TopRealEstate project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+sys.path.append('/root/TopRealEstate')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TopRealEstate.settings')
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
